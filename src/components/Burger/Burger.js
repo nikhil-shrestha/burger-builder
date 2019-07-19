ca0eb8d88df914172511f8709d94b57b07fbc064
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-19 07:08:03 +0545

    starting the burger componets

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export default function Burger() {
-  return <div />;
-}
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+import './Burger.css';
+
+const burger = props => {
+  return (
+    <div className="Burger">
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="salad" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
