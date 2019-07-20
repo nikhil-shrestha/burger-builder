6288b2624e48faa117ab5e71e19071f15adc37d9
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-20 11:23:45 +0545

    fixing common validation gotcha

--- src/container/Checkout/ContactData/ContactData.js
+++ src/container/Checkout/ContactData/ContactData.js
@@ -115,18 +115,18 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
 
     if (rules.required) {
-      isValid = value.trim() !== '';
+      isValid = value.trim() !== '' && isValid;
     }
 
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
 
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      isValid = value.length <= rules.maxLength && isValid;
     }
     return isValid;
   }
@@ -142,6 +142,8 @@ class ContactData extends Component {
     );
 
     updatedOrderForm[inputIdentifier] = updatedFormEle;
+
+    console.log(updatedFormEle);
 
     this.setState({ orderForm: updatedOrderForm });
   };
