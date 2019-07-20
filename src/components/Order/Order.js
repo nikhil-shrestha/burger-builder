f13c5108a61843f533fc373e6f5a7055b1d18b66
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-20 09:38:47 +0545

    adding orders page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Order.css';
+
+export default function Order() {
+  return (
+    <div className="Order">
+      <p>Ingredients: Salad (1)</p>
+      <p>
+        Price: <strong>USD 5.45</strong>
+      </p>
+    </div>
+  );
+}
